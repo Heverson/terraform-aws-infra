019ce571762bc01e0926175fe6a6539d4cebd3c6
Heverson <heversondamasceno@gmail.com>
2022-09-07 14:21:36 -0300

    feat: add database

---
+++ database.tf
@@ -0,0 +1,40 @@
+resource "aws_dynamodb_table" "dynamodb-homologacao" {
+  provider = aws.us-east-2
+  billing_mode   = "PAY_PER_REQUEST"
+  name             = "GameScores"
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+
+  attribute {
+    name = "TopScore"
+    type = "N"
+  }
+  
+  
+
+  global_secondary_index {
+    name               = "GameTitleIndex"
+    hash_key           = "GameTitle"
+    range_key          = "TopScore"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["UserId"]
+  }
+
+  tags = {
+    Name        = "dynamodb-homologacao"
+    Environment = "production"
+  }
+
+}
